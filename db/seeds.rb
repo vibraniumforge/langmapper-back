03f1cf7140abccd4f345a0bffee6fb73254b1bd3
vibraniumforge <gregm9999@gmail.com>
2020-02-23 17:56:42 -0800

    refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Language.destroy_all
+Word.destroy_all
+Translation.destroy_all
+
+
+Language.create({name: "English", abbreviation: "en", alphabet: "Latn", family: "Indo-European", subfamily: "West Germanic", area: "Europe"})
+Language.create({name: "French", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+Language.create({name: "Spanish", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+Language.create({name: "French", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+Language.create({name: "French", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+Language.create({name: "French", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+Language.create({name: "French", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+Language.create({name: "French", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+Language.create({name: "French", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+Language.create({name: "French", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+Language.create({name: "French", abbreviation: "fr", alphabet: "Latn", family: "Indo-European", subfamily: "Romance", area: "Europe"})
+
+Word.create({name: "gold"})
+
+
+
+
+  create_table "translations", force: :cascade do |t|
+    t.bigint "language_id", null: false
+    t.bigint "word_id", null: false
+    t.string "translation"
+    t.string "romanization"
+    t.string "link"
+    t.string "etymology"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["language_id"], name: "index_translations_on_language_id"
+    t.index ["word_id"], name: "index_translations_on_word_id"
+  end
+
