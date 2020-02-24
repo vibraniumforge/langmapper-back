7e1e6f1fbf4d5c265b4c166d22077ffd6ac9f7b2
vibraniumforge <gregm9999@gmail.com>
2020-02-23 21:08:56 -0800

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :translations
-  resources :languages
   namespace :api do
     namespace :v1 do
+      resources :translations
+      resources :languages
       resources :words
-            end
+    end
   end
 end
 
