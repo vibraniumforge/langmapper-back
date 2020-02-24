5161dcce1f41fa5b281b4b9a54233591786b5e61
vibraniumforge <gregm9999@gmail.com>
2020-02-23 17:03:09 -0800

    moved

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :words
       resources :translations
       resources :languages
-      resources :words
     end
   end
 end
+
