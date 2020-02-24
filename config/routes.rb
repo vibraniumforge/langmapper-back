2d23ff54fa21aacd342af8c4c1a075cba9936761
vibraniumforge <gregm9999@gmail.com>
2020-02-23 17:56:35 -0800

    namespaced

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  resources :translations
+  resources :languages
   namespace :api do
     namespace :v1 do
       resources :words
-      resources :translations
-      resources :languages
-    end
+            end
   end
 end
 
