743a53b85a4f60bc305381eddb690bbf19adf383
vibraniumforge <gregm9999@gmail.com>
2020-02-25 15:41:48 -0800

    added dependent destroy

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,4 @@
 class Language < ApplicationRecord
-  has_many :translations
+  has_many :translations, dependent: :destroy
   has_many :words, through: :translations
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,4 @@
 class Word < ApplicationRecord
-  has_many :translations
+  has_many :translations, dependent: :destroy
   has_many :languages, through: :translations
 end
