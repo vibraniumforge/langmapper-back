1a356475fbc7e9b8b996d5c5c540a8d2d0e37417
github username <gregm9999@gmail.com>
2020-02-25 22:48:00 -0800

    added langauge name

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 2020_02_24_063146) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_02_26_034032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,13 +34,12 @@ ActiveRecord::Schema.define(version: 2020_02_24_063146) do
   create_table "translations", force: :cascade do |t|
     t.bigint "language_id", null: false
     t.bigint "word_id", null: false
+    t.string "language_name"
     t.string "translation"
     t.string "romanization"
     t.string "link"
     t.string "gender"
     t.string "etymology"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.index ["language_id"], name: "index_translations_on_language_id"
     t.index ["word_id"], name: "index_translations_on_word_id"
   end
