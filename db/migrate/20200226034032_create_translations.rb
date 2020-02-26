56910faee383287c5c8da4f5cc3ea0ff4fbd751e
github username <gregm9999@gmail.com>
2020-02-25 22:21:55 -0800

    new migration column

--- db/migrate/20200226034032_create_translations.rb
+++ db/migrate/20200226034032_create_translations.rb
@@ -3,13 +3,12 @@ class CreateTranslations < ActiveRecord::Migration[6.0]
     create_table :translations do |t|
       t.references :language, null: false, foreign_key: true
       t.references :word, null: false, foreign_key: true
+      t.string :language_name
       t.string :translation
       t.string :romanization
       t.string :link
       t.string :gender
       t.string :etymology
-      t.datetime :created_at
-      t.datetime :updated_at
     end
   end
 end
