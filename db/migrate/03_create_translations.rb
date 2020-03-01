51489bac833b6f5fc2ba8bc0437b40bd47629998
github username <gregm9999@gmail.com>
2020-03-01 15:18:17 -0800

    new migrations

--- db/migrate/03_create_translations.rb
+++ db/migrate/03_create_translations.rb
@@ -1,14 +1,15 @@
 class CreateTranslations < ActiveRecord::Migration[6.0]
   def change
     create_table :translations do |t|
-      t.references :language, null: false, foreign_key: true
-      t.references :word, null: false, foreign_key: true
+      t.integer :language_id, null: false, foreign_key: true
+      t.integer :word_id, null: false, foreign_key: true
       t.string :language_name
       t.string :translation
       t.string :romanization
       t.string :link
       t.string :gender
       t.string :etymology
+      t.timestamps
     end
   end
 end
