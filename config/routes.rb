055bccc4bbf60bbf09cc12b2af9400ccbc777709
vibraniumforge <gregm9999@gmail.com>
2020-03-03 13:57:58 -0800

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       resources :translations
       resources :languages
       resources :words
+      get '/search/etymology/:word', to: 'translations#find_all_translations', as: 'find_translations_path'
+      get '/search/gender/:word', to: 'translations#find_all_genders', as: 'find_genders_path'
       get '/search/:word', to: 'translations#search', as: 'search_path'
     end
   end
