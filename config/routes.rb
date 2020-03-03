95f46cd5f056745521be5ece2908b172be3b6c8e
vibraniumforge <gregm9999@gmail.com>
2020-03-03 15:21:53 -0800

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
       resources :translations
       resources :languages
       resources :words
-      get '/search/etymology/:word', to: 'translations#find_all_translations', as: 'find_translations_path'
+      get '/search/translation/:word', to: 'translations#find_all_translations', as: 'find_translations_path'
       get '/search/gender/:word', to: 'translations#find_all_genders', as: 'find_genders_path'
-      get '/search/:word', to: 'translations#search', as: 'search_path'
+      get '/search/etymology/:word', to: 'translations#find_etymology_containing', as: 'find_etymology_path'
     end
   end
 end
