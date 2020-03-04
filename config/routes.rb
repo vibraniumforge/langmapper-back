219a35f76c44f29f55cc6f9ff222deeaee0b8598
vibraniumforge <gregm9999@gmail.com>
2020-03-03 23:05:50 -0800

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
       get '/search/gender/:word', to: 'translations#find_all_genders'
       get '/search/etymology/:word', to: 'translations#find_etymology_containing'
       get '/search/grouped_etymology/:word/:macrofamily', to: 'translations#find_grouped_etymologies'
-      get '/search/all_macrofamily_names', to: 'languages#find_all_macrofamilies'
+      get '/search/all_by_macrofamily/:macrofamily', to: 'translations#all_languages_by_macrofamily'
+      get '/search/grouped_etymology/:word/:macrofamily', to: 'translations#find_grouped_etymologies'
+
+      get '/search/all_macrofamily_names', to: 'languages#find_all_macrofamily_names'
     end
   end
 end
