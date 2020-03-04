4855b07f5bea68c858e8ff5018dcaf8c7b08fff3
vibraniumforge <gregm9999@gmail.com>
2020-03-04 14:02:54 -0800

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
       get '/search/gender/:word', to: 'translations#find_all_genders'
       get '/search/etymology/:word', to: 'translations#find_etymology_containing'
       get '/search/grouped_etymology/:word/:macrofamily', to: 'translations#find_grouped_etymologies'
-      get '/search/all_by_macrofamily/:macrofamily', to: 'translations#all_languages_by_macrofamily'
-      get '/search/grouped_etymology/:word/:macrofamily', to: 'translations#find_grouped_etymologies'
+      get '/search/all_by_macrofamily/:macrofamily', to: 'translations#find_all_translations_by_macrofamily'
+
 
       get '/search/all_macrofamily_names', to: 'languages#find_all_macrofamily_names'
     end
