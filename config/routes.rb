93202cdbf451bd0c87a15508f101a8baad39da1f
github username <gregm9999@gmail.com>
2020-03-06 22:34:51 -0800

    added 1 route

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,11 @@ Rails.application.routes.draw do
       get '/search/gender/:word', to: 'translations#find_all_genders'
       get '/search/etymology/:word', to: 'translations#find_etymology_containing'
       get '/search/grouped_etymology/:word/:macrofamily', to: 'translations#find_grouped_etymologies'
-      get '/search/all_by_macrofamily/:macrofamily', to: 'translations#find_all_translations_by_macrofamily'
-
+      get '/search/all_translations_by_macrofamily/:macrofamily', to: 'translations#find_all_translations_by_macrofamily'
+      get '/search/all_translations_by_language/:language', to: 'translations#find_all_translations_by_language'
 
       get '/search/all_macrofamily_names', to: 'languages#find_all_macrofamily_names'
+      get '/search/all_word_names', to: 'words#find_all_word_names'
     end
   end
 end
