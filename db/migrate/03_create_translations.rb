8f1666a2a22a859fc7ba32a7dc1d356b1bd0c01d
github username <gregm9999@gmail.com>
2020-03-07 22:32:36 -0800

    refactor

--- db/migrate/03_create_translations.rb
+++ db/migrate/03_create_translations.rb
@@ -3,12 +3,11 @@ class CreateTranslations < ActiveRecord::Migration[6.0]
     create_table :translations do |t|
       t.integer :language_id, null: false, foreign_key: true
       t.integer :word_id, null: false, foreign_key: true
-      t.string :language_name
       t.string :translation
       t.string :romanization
       t.string :link
       t.string :gender
-      t.string :etymology
+      t.text :etymology
       t.timestamps
     end
   end
