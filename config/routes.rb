aba1295ac10344b7139b115d689c8e3ab8f7ad44
github username <gregm9999@gmail.com>
2020-03-07 20:25:51 -0800

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :translations
       resources :languages
-      resources :words
+      resources :words, only:[:index, :show, :new, :create, :destroy]
+      # no ability to update a word. This leads to bad data.
+      resources :translations, only:[:index, :show, :new, :create, :edit, :update, :destroy]
+
       get '/search/translation/:word', to: 'translations#find_all_translations'
       get '/search/gender/:word', to: 'translations#find_all_genders'
       get '/search/etymology/:word', to: 'translations#find_etymology_containing'
@@ -12,7 +14,12 @@ Rails.application.routes.draw do
       get '/search/all_translations_by_language/:language', to: 'translations#find_all_translations_by_language'
 
       get '/search/all_macrofamily_names', to: 'languages#find_all_macrofamily_names'
-      get '/search/all_word_names', to: 'words#find_all_word_names'
+      get '/search/all_word_names', to: 'words#find_all_words'
+      get '/search/all_alphabet_names', to: 'languages#find_all_alphabets'
+
+      get '/search/language_count', to: 'languages#language_count'
+      get '/search/word_count', to: 'words#word_count'
+      get '/search/translation_count', to: 'translations#translation_count'
     end
   end
 end
