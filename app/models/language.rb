dbb6fbf4f4c5803987e4edc7b13dae4cfd51ec21
github username <gregm9999@gmail.com>
2020-03-07 22:32:02 -0800

    added validations

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,22 @@
 class Language < ApplicationRecord
   has_many :translations, dependent: :destroy
   has_many :words, through: :translations
+
+  validates :name, presence: true
+  validates :abbreviation, length: { in: 2..3 }
+  validates :alive, inclusion: { in: %w(t f) }
+
+  # t.string "name"
+  # t.string "abbreviation"
+  # t.string "alphabet"
+  # t.string "macrofamily"
+  # t.string "family"
+  # t.string "subfamily"
+  # t.string "area"
+  # t.string "area2"
+  # t.string "area3"
+  # t.string "notes"
+  # t.boolean "alive"
+
+
 end
