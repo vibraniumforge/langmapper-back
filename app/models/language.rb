58fbdd3a8ac0cdab6119b05d2088aa4e73b331ef
github username <gregm9999@gmail.com>
2020-03-08 18:26:42 -0700

    added validation logic

--- app/models/language.rb
+++ app/models/language.rb
@@ -3,8 +3,8 @@ class Language < ApplicationRecord
   has_many :words, through: :translations
 
   validates :name, presence: true
-  validates :abbreviation, length: { in: 2..3 }
-  validates :alive, inclusion: { in: %w(t f) }
+  validates :abbreviation, length: { in: 2..3 }, allow_blank: true
+  validates :alive, inclusion: { in: [true, false] }, allow_blank: true
 
   # t.string "name"
   # t.string "abbreviation"
