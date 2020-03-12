73942550e4d8d77811f2ea27013c136433e8b55d
vibraniumforge <gregm9999@gmail.com>
2020-03-12 16:22:39 -0700

    2 new routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,12 @@ Rails.application.routes.draw do
       get '/search/grouped_etymology/:word/:macrofamily', to: 'translations#find_grouped_etymologies'
       get '/search/all_translations_by_macrofamily/:macrofamily', to: 'translations#find_all_translations_by_macrofamily'
       get '/search/all_translations_by_language/:language', to: 'translations#find_all_translations_by_language'
+      get '/search/all_languages_by_area/:location', to: 'languages#find_all_languages_by_area'
 
       get '/search/all_macrofamily_names', to: 'languages#find_all_macrofamily_names'
       get '/search/all_word_names', to: 'words#find_all_words'
       get '/search/all_alphabet_names', to: 'languages#find_all_alphabets'
+      get '/search/all_areas', to: 'languages#find_all_areas'
 
       get '/search/language_count', to: 'languages#language_count'
       get '/search/word_count', to: 'words#word_count'
