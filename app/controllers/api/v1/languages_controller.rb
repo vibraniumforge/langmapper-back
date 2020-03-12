39197a20ecf8058985cfd02a631c1dca586ccff2
vibraniumforge <gregm9999@gmail.com>
2020-03-12 16:21:46 -0700

    2 new methods plus refactor

--- app/controllers/api/v1/languages_controller.rb
+++ app/controllers/api/v1/languages_controller.rb
@@ -1,7 +1,6 @@
 module Api::V1
   class LanguagesController < ApplicationController
-
-    def index 
+    def index
       @languages = Language.all
       render json: @languages
     end
@@ -51,14 +50,16 @@ module Api::V1
       find_language
       if @language.destroy
         render json: { message: "Language successfully deleted.", success: true, data: @language }, status: 200
-      else 
-        render json: { message: "Language NOT successfully deleted.", success:false, data: @language.errors.full_messages.join(", ") }, status: 406
+      else
+        render json: { message: "Language NOT successfully deleted.", success: false, data: @language.errors.full_messages.join(", ") }, status: 406
         puts "Error in delete: #{@language.errors.full_messages.join(", ")}"
       end
     end
 
+    # # # # # # # # # # # #
+
     def find_all_macrofamily_names
-      @macrofamilies = Language.select(:macrofamily).distinct.order(:macrofamily)
+      @macrofamilies = Language.select(:macrofamily).distinct.order(:macrofamily).pluck(:macrofamily)
       render json: { message: "Macrofamilies successfully returned.", success: true, data: @macrofamilies }, status: 200
     end
 
@@ -72,6 +73,21 @@ module Api::V1
       render json: { message: "Languages count successfully returned.", success: true, data: @languages }, status: 200
     end
 
+    def find_all_languages_by_area
+      @languages = Language.where("area = ?", params[:location]).or(Language.where("area2 = ?", params[:location])).or(Language.where("area3 = ?", params[:location]))
+      render json: { message: "Languages successfully returned.", success: true, data: @languages }, status: 200
+    end
+
+    def find_all_areas
+      @areas_ar = []
+      @areas_ar << Language.select(:area).distinct.pluck(:area)
+      # needs to be area1 in the futute
+      @areas_ar << Language.select(:area2).distinct.pluck(:area2)
+      @areas_ar << Language.select(:area3).distinct.pluck(:area3)
+      @areas = @areas_ar.flatten.compact.uniq.sort
+      render json: { message: "Language areas successfully returned.", success: true, data: @areas }, status: 200
+    end
+
     private
 
     def find_language
@@ -81,6 +97,5 @@ module Api::V1
     def language_params
       params.require(:language).permit(:name, :abbreviation, :alphabet, :macrofamily, :family, :subfamily, :area, :area2, :notes, :alive)
     end
-
   end
 end
