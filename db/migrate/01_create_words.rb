79198ee1e578284d60cbeb6314e52f77aa43fbca
github username <gregm9999@gmail.com>
2020-03-17 22:52:25 -0700

    rename name to word_name and addd definition

--- db/migrate/01_create_words.rb
+++ db/migrate/01_create_words.rb
@@ -1,7 +1,8 @@
 class CreateWords < ActiveRecord::Migration[6.0]
   def change
     create_table :words do |t|
-      t.string :name
+      t.string :word_name
+      t.string :definition
 
       t.timestamps
     end
