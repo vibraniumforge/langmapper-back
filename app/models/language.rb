f7b5b654925ffb302a9d649ee0af3087f8c1e550
github username <gregm9999@gmail.com>
2020-03-17 22:52:01 -0700

    cleanup

--- app/models/language.rb
+++ app/models/language.rb
@@ -6,17 +6,4 @@ class Language < ApplicationRecord
   validates :abbreviation, length: { in: 2..3 }, allow_blank: true
   validates :alive, inclusion: { in: [true, false] }, allow_blank: true
 
-  # t.string "name"
-  # t.string "abbreviation"
-  # t.string "alphabet"
-  # t.string "macrofamily"
-  # t.string "family"
-  # t.string "subfamily"
-  # t.string "area"
-  # t.string "area2"
-  # t.string "area3"
-  # t.string "notes"
-  # t.boolean "alive"
-
-
 end
