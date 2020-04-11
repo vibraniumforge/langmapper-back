e3cc23cd31f7c809c7a8af31d80da5ef13257b6e
vibraniumforge <gregm9999@gmail.com>
2020-04-11 14:58:35 -0700

    new method for faster lang search

--- app/models/language.rb
+++ app/models/language.rb
@@ -6,4 +6,11 @@ class Language < ApplicationRecord
   validates :abbreviation, length: { in: 2..3 }, allow_blank: true
   validates :alive, inclusion: { in: [true, false] }, allow_blank: true
 
+  def self.current_langauges_hash
+    all_langs = Language.all.pluck(:id, :name)
+    all_langs.map do |lang|
+      Hash[id: lang[0], name: lang[1]]
+    end
+  end
+
 end
