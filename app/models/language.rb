d8fe486947229cb5351c533bcb688f55e79940a3
vibraniumforge <gregm9999@gmail.com>
2020-07-01 13:55:30 -0700

    added logic from controller

--- app/models/language.rb
+++ app/models/language.rb
@@ -13,4 +13,28 @@ class Language < ApplicationRecord
     end
   end
 
+  # helpers
+
+  def self.find_all_macrofamily_names
+    Language.select(:macrofamily).distinct.order(:macrofamily).pluck(:macrofamily)
+  end
+
+  def self.find_all_alphabet_names
+    Language.select(:alphabet).distinct.order(:alphabet).pluck(:alphabet)
+  end
+
+  def self.find_all_area_names
+    Language.select(:area1, :area2, :area3).distinct.pluck(:area1, :area2, :area3).flatten.uniq.reject{|x|x.blank?}.sort
+  end
+
+  # searchers
+  def self.find_all_languages_by_area(area)
+    Language.where("area1 = ?", area).or(Language.where("area2 = ?", area)).or(Language.where("area3 = ?", area))
+  end
+
+  # counter
+  def self.languages_count
+    Languages.count
+  end
+
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -3,4 +3,17 @@ class Word < ApplicationRecord
   has_many :languages, through: :translations
 
   validates :word_name, presence: true
+
+  def self.find_all_word_names
+    Word.all.order(:word_name)
+  end
+
+  def self.words_count
+    Word.count
+  end
+
+  def self.find_word_definition(word)
+    Word.where(word_name: word).pluck(:definition)
+  end
+
 end
