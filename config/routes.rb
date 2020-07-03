26a6abd1ecb6e6d8b75a539733954a0d3191daf8
vibraniumforge <gregm9999@gmail.com>
2020-07-03 15:31:36 -0700

    renamed routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,19 +20,17 @@ Rails.application.routes.draw do
       # ADMIN: :index, :show, :edit, :update, :destroy
       # Admin can't create. Only info from wiktionary.
 
-      # create maps
-      get "/search/all_translations_by_area/:location/:word", to: "translations#find_all_translations_by_area"
-      # only the above for now. This one route is good enough for the three maps. Displays all info in the table.
+      # Custom routes
+
+      # NAMING
+      # /search searches for something and takes 1 or more params
+      # /get returns all.
+      # /get || /search /controller/method to
 
       # create images
       get "/search/all_translations_by_area_img/:location/:word", to: "translations#find_all_translations_by_area_img"
       get "/search/all_etymologies_by_area_img/:location/:word", to: "translations#find_all_etymologies_by_area_img"
       get "/search/all_genders_by_area_img/:location/:word", to: "translations#find_all_genders_by_area_img"
-
-      # NAMING
-      # /search searches for something. takes 1 or more params
-      # /get returns all.
-      # /get || /search /controller/method to
 
       # REFACTORED BELOW HERE
 
@@ -46,19 +44,22 @@ Rails.application.routes.draw do
       # searchers
       get "/search/words/definition/:word", to: "words#find_word_definition"
 
-      get "/search/languages/languages_by_area/:area", to: "languages#find_all_languages_by_area"
+      get "/search/languages/area/:area", to: "languages#find_all_languages_by_area"
 
       get "/search/translations/language/:language", to: "translations#find_all_translations_by_language"
-      get "/search/translations/word/:word", to: "translations#search_all_translations_by_word"
-      get "/search/translations/gender/:word", to: "translations#find_all_genders"
+      get "/search/translations/word/:word", to: "translations#find_all_translations_by_word"
+      get "/search/translations/gender/:word", to: "translations#find_all_translations_by_gender"
       get "/search/translations/etymology/:word", to: "translations#find_etymology_containing"
+      get "/search/translations/area/:area/:word", to: "translations#find_all_translations_by_area"
+      # This route does the "Search Translations by Area" route.
+      # It also gets the data for all 3 maps's tables
 
       # helpers
-      get "/get/languages/macrofamily_names", to: "languages#find_all_macrofamily_names"
-      get "/get/languages/alphabet_names", to: "languages#find_all_alphabet_names"
-      get "/get/languages/area_names", to: "languages#find_all_area_names"
+      get "/get/languages/macrofamily_names", to: "languages#all_macrofamily_names"
+      get "/get/languages/alphabet_names", to: "languages#all_alphabet_names"
+      get "/get/languages/area_names", to: "languages#all_area_names"
 
-      get "/get/words/word_names", to: "words#find_all_word_names"
+      get "/get/words/word_names", to: "words#all_word_names"
 
       # counters
       get "/get/languages/languages_count", to: "languages#languages_count"
