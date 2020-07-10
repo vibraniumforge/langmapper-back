24925f9559d641957626ab263a261cd236273390
vibraniumforge <gregm9999@gmail.com>
2020-07-10 12:05:41 -0700

    first commit

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,35 @@
+module Api::V1
+  class AuthController < ApplicationController
+    # skip_before_action :authorized, only: [:login]
+
+    def show
+      @user = current_user
+      if logged_in?
+        render json: {message: "Logged in", success: true, data: @user}
+      else
+        render json: {message: "No user found", success: false }
+      end
+    end
+
+    def login
+      puts "in login"
+      @user = User.find_by(name: params[:user][:name])
+      if @user && @user.authenticate(params[:user][:password])
+        token = encode_token({ user_id: @user.id})
+        render json: {message: "Success", success: true, data: @user, jwt: token }
+        puts "LOGGED IN"
+        puts token
+      else
+        puts "Invalid username or password"
+        render json: {message: "Invalid username or password", success: false }
+      end
+    end
+
+    private
+
+    def user_login_params
+      params.require(:auth).permit(:username, :password)
+    end
+
+  end
+end
