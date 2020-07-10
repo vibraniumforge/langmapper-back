a880afb002acaa0c3a3c387c82cdd7c52bf782ac
vibraniumforge <gregm9999@gmail.com>
2020-07-10 12:04:24 -0700

    added info

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://arcane-wave-94268.herokuapp.com/', 'http://localhost:3000'
+    # origins '*'
 
     resource '*',
       headers: :any,
