8bc07d5e292adabc72d4f6dec49e47613ec35333
vibraniumforge <gregm9999@gmail.com>
2020-07-10 19:28:06 -0700

    typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://arcane-wave-94268.herokuapp.com/', 'http://localhost:3000'
+    origins 'https://arcane-wave-94268.herokuapp.com', 'http://localhost:3000'
     # origins '*'
 
     resource '*',
