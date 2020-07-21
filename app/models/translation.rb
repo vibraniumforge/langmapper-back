40a8c046cd0c4224f9058b943b1c2aa68d64a387
vibraniumforge <gregm9999@gmail.com>
2020-07-20 18:10:39 -0700

    get macrofamily

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -10,7 +10,7 @@ class Translation < ApplicationRecord
   # Find all translations of a WORD in ALL LANGUAGES
   def self.find_all_translations_by_word(query)
     word_id = Word.find_by("word_name = ?", query.downcase).id
-    Translation.joins(:language, :word).select("translations.*, languages.name, words.definition").where(word_id: word_id).order(:name)
+    Translation.joins(:language, :word).select("translations.*, languages.name, languages.macrofamily, words.definition").where(word_id: word_id).order(:name)
   end
 
   # all translations of a WORD in a MACROFAMILY. Gender is inside
@@ -21,18 +21,18 @@ class Translation < ApplicationRecord
 
   # Translations that contain the query word inside the Translation etymology.
   def self.find_etymology_containing(query)
-    Translation.joins(:language, :word).select("translations.*, languages.name, words.word_name ").where("etymology LIKE :query", query: "%#{sanitize_sql_like(query)}%")
+    Translation.joins(:language, :word).select("translations.*, languages.name, languages.macrofamily, words.word_name ").where("etymology LIKE :query", query: "%#{sanitize_sql_like(query)}%")
   end
 
     # all the translations of EVERY WORD in a MACROFAMILY
     def self.find_all_translations_by_macrofamily(macrofamily)
-      Translation.joins(:language, :word).select("translations.*, languages.*, words.word_name").where("macrofamily = ?", macrofamily).order(:family)
+      Translation.joins(:language, :word).select("translations.*, languages.*, words.word_name as word_name").where("macrofamily = ?", macrofamily).order(:family)
     end
   
     # all the TRANSLATIONS from a specified LANGUAGE
     def self.find_all_translations_by_language(language)
       language_id = Language.find_by(name: language).id
-      Translation.joins(:word).select("translations.*, words.word_name").where("language_id = ?", language_id).order(:romanization)
+      Translation.joins(:language, :word).select("translations.*, languages.macrofamily, words.word_name").where("language_id = ?", language_id).order(:romanization)
     end
   
     # find all the translations of the word_name && are in area1 || area2 || area3.
