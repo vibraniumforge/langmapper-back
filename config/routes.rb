31c5c1498de6ba4afc0fcbc222a9040791b8cd9d
vibraniumforge <gregm9999@gmail.com>
2020-08-06 11:00:12 -0700

    cleanup, rename map routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "welcome#index"
+  
   namespace :api do
     namespace :v1 do
       resources :languages, only: [:index, :show, :new, :create, :edit, :update, :destroy]
@@ -23,7 +24,9 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :show]
       # ADMIN can only see and show a user. User is admin. Should only be one.
 
-      post "/auth/login/", to: "auth#login"
+      post "/auth/login", to: "auth#login"
+      post "/auth", to: "application#authorized"
+      # above needs to go outside api/v1 namespace.
 
       
       # Custom routes
@@ -35,62 +38,44 @@ Rails.application.routes.draw do
 
       # Used
 
-
-
       # create images
-      get "/search/all_translations_by_area_img/:area/:word", to: "translations#find_all_translations_by_area_img"
-      get "/search/all_etymologies_by_area_img/:area/:word", to: "translations#find_all_etymologies_by_area_img"
-      get "/search/all_genders_by_area_img/:area/:word", to: "translations#find_all_genders_by_area_img"
+      get "/translations/search/all_translations_by_area_img/:area/:word", to: "translations#find_all_translations_by_area_img"
+      get "/translations/search/all_etymologies_by_area_img/:area/:word", to: "translations#find_all_etymologies_by_area_img"
+      get "/translations/search/all_genders_by_area_img/:area/:word", to: "translations#find_all_genders_by_area_img"
 
+      # below is the way to show ONLY what is on the first Europe map
+      get "/translations/search/area_europe_map/:area/:word", to: "translations#find_all_translations_by_area_europe_map"
+ 
       # searchers
       
       get "/words/search/definition/:word", to: "words#find_word_definition"
-      # renamed above
 
       get "/languages/search/area/:area", to: "languages#find_all_languages_by_area"
-      # renamed above
-      
-      get "/search/grouped_etymology/:word/:macrofamily", to: "translations#find_grouped_etymologies"
-      # renamed above
 
       get "/translations/search/macrofamily/:macrofamily", to: "translations#find_all_translations_by_macrofamily"
-      # renamed above
       get "/translations/search/language/:language", to: "translations#find_all_translations_by_language"
-      # renamed above
       get "/translations/search/word/:word", to: "translations#find_all_translations_by_word"
-      # renamed above
       get "/translations/search/gender/:word", to: "translations#find_all_translations_by_word_gender"
-      # renamed above
       get "/translations/search/etymology/:word", to: "translations#find_etymology_containing"
-      # renamed above
       get "/translations/search/area/:area/:word", to: "translations#find_all_translations_by_area"
-      # renamed above
       # This above route does the "Search Translations by Area" page route.
       # It also gets the data for all 3 maps' tables. It gets everything that matches, even if it is not on the map.
 
-      # below is the way to show ONLY what is on the map
-      get "/translations/search/area_europe_map/:area/:word", to: "translations#find_all_translations_by_area_europe_map"
-      # renamed above
       get "translations/get/seeds", to: "translations#seeds"
-      # renamed above
 
-      # helpers
+
+      # helpers for selects
       get "/languages/get/macrofamily_names", to: "languages#all_macrofamily_names"
-       # renamed above
-      get "languages/get/alphabet_names", to: "languages#all_alphabet_names"
-       # renamed above
+      get "/languages/get/alphabet_names", to: "languages#all_alphabet_names"
       get "/languages/get/area_names", to: "languages#all_area_names"
-      # renamed above
+
       get "words/get/word_names", to: "words#all_word_names"
-      # renamed above
 
       # counters
       get "/languages/get/languages_count", to: "languages#languages_count"
-      # renamed above
       get "/translations/get/translations_count", to: "translations#translations_count"
-      # renamed above
       get "/words/get/words_count", to: "words#words_count"
-      # renamed above
+
     end
   end
 end
