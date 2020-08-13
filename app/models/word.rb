f683b8515bd1636dca5535588c123d8175d2339a
vibraniumforge <gregm9999@gmail.com>
2020-08-13 12:33:45 -0700

    add a validation

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,7 +3,11 @@ class Word < ApplicationRecord
   has_many :translations, dependent: :destroy
   has_many :languages, through: :translations
 
-  validates :word_name, presence: true
+  validates :word_name, presence: true, uniqueness: true
+
+  Good_words = ["Silver", "Gold", "Copper", "Iron", "Tin", "Salt", "Horse", "Cow", "Sheep", "Pig", "Dog", "Swan", "Goose", "Owl", "Falcon", "Crow", "Dove", "Eagle", "Gull", "Robin", "Wolf", "Fox", "Bear", "Mouse", "Honey", "Apple", "Milk", "Egg", "Tree", "Snow", "Rain", "Cloud", "Ice", "Frost", "Blood", "Tongue", "Tooth", "Ear", "Eye", "Nose", "Hand", "Wheel", "Day", "Dawn", "Night", "Winter", "Summer", "Autumn", "Sun", "Moon", "Sky", "Star", "Water", "Sea"]
+
+  Good_word = ["Flower"]
 
   def self.all_word_names
     # Word.select("words.word_name, words.id").order("word_name ASC")
@@ -17,4 +21,5 @@ class Word < ApplicationRecord
   def self.find_word_definition(word)
     Word.where(word_name: word).pluck(:definition)
   end
+
 end
