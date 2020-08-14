5879ce9d6654c15d26a130d67b73686ea7dd3f7e
vibraniumforge <gregm9999@gmail.com>
2020-08-14 00:24:52 -0700

    cleanup

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,10 +5,6 @@ class Word < ApplicationRecord
 
   validates :word_name, presence: true, uniqueness: true
 
-  Good_words = ["Silver", "Gold", "Copper", "Iron", "Tin", "Salt", "Horse", "Cow", "Sheep", "Pig", "Dog", "Swan", "Goose", "Owl", "Falcon", "Crow", "Dove", "Eagle", "Gull", "Robin", "Wolf", "Fox", "Bear", "Mouse", "Honey", "Apple", "Milk", "Egg", "Tree", "Snow", "Rain", "Cloud", "Ice", "Frost", "Blood", "Tongue", "Tooth", "Ear", "Eye", "Nose", "Hand", "Wheel", "Day", "Dawn", "Night", "Winter", "Summer", "Autumn", "Sun", "Moon", "Sky", "Star", "Water", "Sea"]
-
-  Good_word = ["Flower"]
-
   def self.all_word_names
     # Word.select("words.word_name, words.id").order("word_name ASC")
     Word.select("words.word_name, words.id").order(word_name: :asc)
